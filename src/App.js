fd718cb08eb1706eb8df1eb211e85b435622495d
William Niemiec <williamniemiec@hotmail.com>
2022-08-12 19:06:16 -0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+/*
+ * Copyright (c) William Niemiec.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import React from "react";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Routes from "./routes";
 
+
+//-----------------------------------------------------------------------------
+//        Components
+//-----------------------------------------------------------------------------
 const App = () => (
   <>
     <ToastContainer position="top-right" />
