86b712747f627675fdb470c6181717ea7a32e65e
William Niemiec <williamniemiec@hotmail.com>
2022-09-07 21:00:04 -0300

    ApiConfig

---
+++ src/config/api.config.ts
@@ -0,0 +1,5 @@
+const ApiConfig = {
+  BASE_URL: 'http://localhost:3002/'
+};
+
+export default ApiConfig;
--- src/services/axios/index.js
+++ src/services/axios/index.js
@@ -1,10 +1,9 @@
+import ApiConfig from '../../config/api.config';
+
 const axios = require('axios');
 
-//const API_URL = 'https://tg2-api.herokuapp.com/'
-const API_URL = 'http://localhost:3002/';
-
 export const remoteRequest = axios.create({
-    baseURL: API_URL,
+    baseURL: ApiConfig.BASE_URL,
     timeout: 10000,
     headers: {'authorization': localStorage.getItem('x_auth_token')}
 });
