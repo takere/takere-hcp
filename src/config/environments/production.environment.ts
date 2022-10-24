b578a78bb472903428c816693d657a63bc50a450
William Niemiec <williamniemiec@hotmail.com>
2022-10-24 12:55:12 -0300

    Timeout - fix

--- src/config/environments/production.environment.ts
+++ src/config/environments/production.environment.ts
@@ -3,7 +3,7 @@ import ApiConfig from "../../models/api-config.model";
 
 const ProductionApiConfig: ApiConfig = {
   BASE_URL: 'https://takere-api.onrender.com/',
-  REQUEST_TIMEOUT: 10000
+  REQUEST_TIMEOUT: 500000
 };
 
 export default ProductionApiConfig;
